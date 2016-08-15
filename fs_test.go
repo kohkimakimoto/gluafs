32db9c2c5bbb6ca3d12f53e1514575589be6c214
kohkimakimoto <kohki.makimoto@gmail.com>
2016-08-15 13:59:07 +0900

    udpate README and add tests

--- fs_test.go
+++ fs_test.go
@@ -112,3 +112,38 @@ assert(ret == nil)
 		t.Error(err)
 	}
 }
+
+
+func TestGlob(t *testing.T) {
+	tmpDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Errorf("should not raise error: %v", err)
+	}
+	defer func() {
+		os.RemoveAll(tmpDir)
+	}()
+
+	L := lua.NewState()
+	defer L.Close()
+
+	L.PreloadModule("fs", Loader)
+	if err := L.DoString(`
+local fs = require("fs")
+fs.mkdir("` + tmpDir + `/dir01")
+fs.mkdir("` + tmpDir + `/dir02")
+fs.mkdir("` + tmpDir + `/dir03")
+fs.write("` + tmpDir + `/file01", "aaaaaaa")
+fs.write("` + tmpDir + `/file02", "bbbbbbb")
+
+local ret, err = fs.glob("` + tmpDir + `/*", function(file)
+	print(file.path)
+	print(file.realpath)
+end)
+
+assert(ret == true)
+assert(err == nil)
+
+	`); err != nil {
+		t.Error(err)
+	}
+}
